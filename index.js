0e8ffac1a8b64c38df367803236eefbad1ac9fbb
banksherman <banksherman8@gmail.com>
2025-08-24 19:15:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9090;
   
   //=============================================
   
