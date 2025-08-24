0d328d927c36765b9ddfd310b2e0495953831749
banksherman <banksherman8@gmail.com>
2025-08-24 11:55:20 +0300

    Delete api/pair.js

--- api/pair.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const pairCode = generatePairCode(); 
-  return res.json({ pairCode });
-}
-
-function generatePairCode() {
-  return Math.random().toString(36).substr(2, 6);
-}
