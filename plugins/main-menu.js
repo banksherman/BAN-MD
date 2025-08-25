f62cc674304dab026839da1b6df97598f883c18b
banksherman <banksherman8@gmail.com>
2025-08-25 08:29:28 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -362,7 +362,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/Arslan-MD/Arslan_MD/blob/main/assets/menu.m4a' },
+            audio: { url: 'https://github.com/banksherman/BAN-MD//blob/main/assets/menu.m4a' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
